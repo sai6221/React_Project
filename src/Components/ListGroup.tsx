f3d2613abd0f8b5f5eff01532f4e48af2a94f585
Sai Reddy <sai6221@gmail.com>
2023-06-27 16:02:52 -0700

    Added State to a button

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -2,6 +2,7 @@ import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Paris"];
+  let selectedIndex = 0;
 
   //Event Handler
   const handleClick = (event: MouseEvent) => console.log(event);
@@ -12,8 +13,16 @@ function ListGroup() {
       {items.length === 0 ? <p>No results found.</p> : null}
       {/* {items.length && <p>No results found.</p>} Alternte way of writing the above without null */}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
