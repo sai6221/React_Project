dde16b39abbd5b39b5d64ac008fac293e44dd61b
Sai Reddy <sai6221@gmail.com>
2023-06-27 15:37:17 -0700

    Added event Listener for mouse click

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,6 +1,10 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Paris"];
-  items = [];
+
+  //Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -9,7 +13,9 @@ function ListGroup() {
       {/* {items.length && <p>No results found.</p>} Alternte way of writing the above without null */}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
