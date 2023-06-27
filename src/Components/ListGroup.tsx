20ac0845c9f102bc9253df754488e069c47d8fb7
Sai Reddy <sai6221@gmail.com>
2023-06-27 12:35:21 -0700

    Made conditional rendering and rendered a list of elements.

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,9 +1,12 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Paris"];
+  let items = ["New York", "San Francisco", "Paris"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No results found.</p> : null}
+      {/* {items.length && <p>No results found.</p>} Alternte way of writing the above without null */}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
