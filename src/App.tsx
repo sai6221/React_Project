d07338ba0492af00922b4a4a702c3efc656ef823
Sai Reddy <sai6221@gmail.com>
2023-06-27 17:12:20 -0700

    Learned React developer Tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import ListGroup from "./Components/ListGroup";
+import Alert from "./Components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>Hello World</Alert>
     </div>
   );
 }
