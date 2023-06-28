45c7774d1497cbb7270a6f8c21480ffafa9a882c
Sai Reddy <sai6221@gmail.com>
2023-06-27 17:24:02 -0700

    Button added with dynamic values

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Alert from "./Components/Alert";
+import Buttons from "./Components/Buttons";
 
 function App() {
   return (
     <div>
       <Alert>Hello World</Alert>
+      <Buttons
+        children="My Button"
+        onClick={() => console.log("Clicked")}
+      ></Buttons>
     </div>
   );
 }
